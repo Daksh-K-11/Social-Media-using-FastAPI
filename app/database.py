463a427507edfd1951ce48a4683e1b294c1232b7
Daksh-K-11 <khinvasara.daksh@gmail.com>
2025-01-01 15:35:45 +0530

    Remove files from tracking that are now in .gitignore

--- app/database.py
+++ app/database.py
@@ -5,13 +5,12 @@ import urllib.parse
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from .config import settings
 
-postgres_user = 'postgres'
-postgres_password = urllib.parse.quote_plus('Chennai@05')
-postgres_host = 'localhost'
-postgres_port = '5432'
-postgres_db_name = 'fastapi'
-postgres_url = f'postgresql://{postgres_user}:{postgres_password}@{postgres_host}/{postgres_db_name}'
+database_password = urllib.parse.quote_plus(settings.database_password)
+
+postgres_url = f'postgresql://{settings.database_username}:{database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+print(postgres_url)
 
 engine = create_engine(postgres_url)
 
@@ -27,14 +26,14 @@ def get_db():
         db.close()
         
 
-while True:    
-    try:
-        con = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
-                            password='Chennai@05', cursor_factory=RealDictCursor)
-        cur = con.cursor()
-        print('DB connection established')
-        break
-    except Exception as e:
-        print("Unable to connect to the database")
-        print("Error: ",e)
-        time.sleep(2)
+# while True:    
+#     try:
+#         con = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
+#                             password='Chennai@05', cursor_factory=RealDictCursor)
+#         cur = con.cursor()
+#         print('DB connection established')
+#         break
+#     except Exception as e:
+#         print("Unable to connect to the database")
+#         print("Error: ",e)
+#         time.sleep(2)
